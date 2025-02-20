6f4f74afac2cbcb15607d47c396d4f28e08ee3f1
stevexero <steveanthony999@gmail.com>
2025-02-20 14:09:38 -0800

    navbar wip

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -1,18 +1,18 @@
 'use client';
 
 import { PiFlowerLotusBold } from 'react-icons/pi';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdDarkMode } from 'react-icons/md';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-const linksLeft = [
+const links = [
   { name: 'Services', href: '/services', icon: PiFlowerLotusBold },
   { name: 'About', href: '/about', icon: PiFlowerLotusBold },
   { name: 'Contact', href: '/contact', icon: PiFlowerLotusBold },
-];
-
-const linksRight = [
   { name: 'Testimonials', href: '/testimonials', icon: PiFlowerLotusBold },
   { name: 'Gift Cards', href: '/gift-cards', icon: PiFlowerLotusBold },
   { name: 'Schedule', href: '/schedule', icon: PiFlowerLotusBold },
@@ -21,55 +21,62 @@ const linksRight = [
 export default function NavLinks() {
   const pathname = usePathname();
 
+  const [theme, setTheme] = useState(true);
+
+  const handleThemeChange = () => {
+    setTheme((prev) => !prev);
+  };
+
+  useEffect(() => {
+    console.log(theme);
+  }, [theme]);
+
   return (
-    <nav className='w-full flex'>
-      {linksLeft.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx(
-              'flex flex-col grow items-center justify-center gap-2 rounded-md p-3 font-medium hover:bg-amber-50 hover:text-amber-500',
-              {
-                'bg-amber-50 text-amber-500': pathname === link.href,
-              }
-            )}
-          >
-            <LinkIcon className='w-6' />
-            <p className='block'>{link.name}</p>
-          </Link>
-        );
-      })}
+    <>
+      <div className='w-full flex items-center justify-evenly py-4'>
+        <div className='flex flex-col items-center justify-center hover:text-amber-500 hover:cursor-pointer'>
+          <GiHamburgerMenu />
+          <p className='block'>More</p>
+        </div>
 
-      <Link href='/'>
-        <Image
-          src='/luxelogo_gold.png'
-          width={305}
-          height={189}
-          className='block'
-          alt='LUXE Logo'
-        />
-      </Link>
+        <Link href='/'>
+          <Image
+            src='/luxelogo_gold.png'
+            width={305}
+            height={189}
+            className='block w-56'
+            alt='LUXE Logo'
+          />
+        </Link>
 
-      {linksRight.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx(
-              'flex flex-col grow items-center justify-center gap-2 rounded-md p-3 font-medium hover:bg-amber-50 hover:text-amber-500',
-              {
-                'bg-amber-50 text-amber-500': pathname === link.href,
-              }
-            )}
-          >
-            <LinkIcon className='w-6' />
-            <p className='hidden md:block'>{link.name}</p>
-          </Link>
-        );
-      })}
-    </nav>
+        <div
+          className='flex flex-col items-center justify-center hover:text-amber-500 hover:cursor-pointer'
+          onClick={handleThemeChange}
+        >
+          <MdDarkMode />
+          <p className='block'>Mode</p>
+        </div>
+      </div>
+
+      <div className='w-full flex justify-center border-t-[0.5px] border-b-[0.5px] border-yellow-500 p-4 sticky top-0'>
+        <nav className='w-3/4 flex justify-between'>
+          {links.map((link) => {
+            const LinkIcon = link.icon;
+            return (
+              <Link
+                key={link.name}
+                href={link.href}
+                className={clsx('flex items-center hover:text-amber-500', {
+                  'bg-amber-50 text-amber-500': pathname === link.href,
+                })}
+              >
+                <LinkIcon />
+                <p className='block ml-2'>{link.name}</p>
+              </Link>
+            );
+          })}
+        </nav>
+      </div>
+    </>
   );
 }
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -2,8 +2,8 @@ import NavLinks from './nav-links';
 
 export default function Navbar() {
   return (
-    <div className='w-full flex px-3 py-4 md:px-2'>
+    <>
       <NavLinks />
-    </div>
+    </>
   );
 }
