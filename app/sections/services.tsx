4b1f1ea99907c09a6fd354d57745c8cd4aa690df
stevexero <steveanthony999@gmail.com>
2025-02-25 18:27:57 -0800

    Added packages

--- app/sections/services.tsx
+++ app/sections/services.tsx
@@ -14,7 +14,7 @@ const cards = [
     pricing120: '160',
   },
   {
-    image: 'a2.png',
+    image: 'a5.png',
     title: 'Swedish Massage',
     description:
       'Muscle manipulation and warm oils to relieve pain, release tension, and promote relaxation through effleurage techniques that soothe aching muscles.',
@@ -47,7 +47,7 @@ const cards = [
     pricing120: '180',
   },
   {
-    image: 'a5.png',
+    image: 'a9.png',
     title: 'Traditional Thai Massage',
     description:
       'A blend of acupressure, deep stretching, and rhythmic compression to enhance flexibility, relieve tension, and restore energy flow. Performed on a mat without oils, it uses guided movements and pressure points to improve circulation and mobility.',
@@ -80,8 +80,8 @@ const cards = [
     pricing120: '320',
   },
   {
-    image: 'a6.png',
-    title: 'Body Scrub / LUXE Combo',
+    image: 'a2.png',
+    title: 'Body Scrub',
     description:
       '* $80 / 30 MINUTES.  A rejuvenating mix of Thai massage and full-body exfoliation with sugar, salt, and essential oils. Your therapist gently polishes your skin, followed by a refreshing shower, leaving you smooth, refreshed, and revitalized.',
     href: 'bodyscrubcombo',
@@ -123,6 +123,72 @@ const Services = () => {
       <h3 className='font-lavishly text-6xl text-center text-textPrimary-dark mt-12'>
         Packages
       </h3>
+      <div className='mt-12 w-full flex flex-col items-center justify-center'>
+        {/* LUXE LUXURY */}
+        <div
+          className='w-full md:w-3/4 2xl:w-1/2 rounded-lg p-8 bg-no-repeat bg-cover bg-center shadow-2xl shadow-primary-dark'
+          style={{ backgroundImage: "url('/goldbg.png')" }}
+        >
+          <h4 className='font-bold text-primary-dark text-4xl text-center drop-shadow-[0_1.2px_0.5px_rgba(255,255,255,1)]'>
+            LUXE <span className='font-lavishly text-5xl'>luxury</span>
+          </h4>
+          <p className='font-bold text-lg text-primary-dark text-center drop-shadow-[0_0.5px_0.25px_rgba(255,255,255,1)] mt-4'>
+            30 minute body scrub. 60 minute massage and hot stones. 30 minutes
+            basic facial.
+          </p>
+          <p className='font-bold text-xl text-primary-dark text-center drop-shadow-[0_1px_0.25px_rgba(255,255,255,1)] mt-4'>
+            $200 / 2 hours
+          </p>
+          <div className='w-full flex items-center justify-center mt-8'>
+            <button className='bg-primary-dark px-8 py-4 text-white rounded-md shadow-2xl shadow-textPrimary hover:scale-105 transition-all duration-200'>
+              Schedule Now
+            </button>
+          </div>
+        </div>
+        {/* LUXE FABULOUS */}
+        <div
+          className='w-full md:w-3/4 2xl:w-1/2 rounded-lg p-8 bg-no-repeat bg-cover bg-center shadow-2xl shadow-primary-dark mt-12'
+          style={{ backgroundImage: "url('/goldbg.png')" }}
+        >
+          <h4 className='font-bold text-primary-dark text-4xl text-center drop-shadow-[0_1.2px_0.5px_rgba(255,255,255,1)]'>
+            LUXE <span className='font-lavishly text-5xl'>fabulous</span>
+          </h4>
+          <p className='font-bold text-lg text-primary-dark text-center drop-shadow-[0_0.5px_0.25px_rgba(255,255,255,1)] mt-4'>
+            60 minute massage and hot stones. 60 minutes basic facial.
+          </p>
+          <p className='font-bold text-xl text-primary-dark text-center drop-shadow-[0_1px_0.25px_rgba(255,255,255,1)] mt-4'>
+            $160 / 2 hours
+          </p>
+          <div className='w-full flex items-center justify-center mt-8'>
+            <button className='bg-primary-dark px-8 py-4 text-white rounded-md shadow-2xl shadow-textPrimary hover:scale-105 transition-all duration-200'>
+              Schedule Now
+            </button>
+          </div>
+        </div>
+        {/* LUXE REJUVENATE */}
+        <div
+          className='w-full md:w-3/4 2xl:w-1/2 rounded-lg p-8 bg-no-repeat bg-cover bg-center shadow-2xl shadow-primary-dark mt-12'
+          style={{ backgroundImage: "url('/goldbg.png')" }}
+        >
+          <h4 className='font-bold text-primary-dark text-4xl text-center drop-shadow-[0_1.2px_0.5px_rgba(255,255,255,1)]'>
+            LUXE <span className='font-lavishly text-5xl'>rejuvenate</span>
+          </h4>
+          <p className='font-bold text-lg text-primary-dark text-center drop-shadow-[0_0.5px_0.25px_rgba(255,255,255,1)] mt-4'>
+            30 minute body scrub. 60 or 90 minute Massage and hot stones.
+          </p>
+          <p className='font-bold text-xl text-primary-dark text-center drop-shadow-[0_1px_0.25px_rgba(255,255,255,1)] mt-4'>
+            $160 / 90 minutes
+          </p>
+          <p className='font-bold text-xl text-primary-dark text-center drop-shadow-[0_1px_0.25px_rgba(255,255,255,1)]'>
+            $190 / 120 minutes
+          </p>
+          <div className='w-full flex items-center justify-center mt-8'>
+            <button className='bg-primary-dark px-8 py-4 text-white rounded-md shadow-2xl shadow-textPrimary hover:scale-105 transition-all duration-200'>
+              Schedule Now
+            </button>
+          </div>
+        </div>
+      </div>
       <div className='mt-12'>
         <Link href='/services' className='text-center'>
           <p className='block text-lg mr-2 font-bold underline text-textPrimary-dark'>
