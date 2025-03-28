0efabcef8987fba39bda7f55f4bf3b8b3f1dca1e
stevexero <steveanthony999@gmail.com>
2025-03-27 21:34:31 -0700

    Deleted excess

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,28 +1,20 @@
 import { Suspense } from 'react';
-import CardWrapper from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import {
-  RevenueChartSkeleton,
-  LatestInvoicesSkeleton,
-  CardsSkeleton,
-} from '@/app/ui/skeletons';
 
 export default async function Page() {
   return (
     <main>
       <h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
       <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
-        <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
+        <Suspense fallback={<p>Loading...</p>}>
+          <h1>Hi</h1>
         </Suspense>
       </div>
       <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
+        <Suspense fallback={<p>Loading...</p>}>
+          <h2>Hi</h2>
         </Suspense>
-        <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
+        <Suspense fallback={<p>Loading...</p>}>
+          <h3>Hi</h3>
         </Suspense>
       </div>
     </main>
